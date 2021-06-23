de66b4a4b56373d78421a7ade493acb540d395a6
Codingshivam2004 <77269075+Codingshivam2004@users.noreply.github.com>
2021-06-23 13:00:30 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "DATABASE";
+const accessCode2 = "FUNCTION";
+
+
+function clues() {
+    
+    fill("lightgreen")
+    textSize(15)
+    text("A T A D A S B A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+fill("red")
+    textSize(15);
+text("N U F I O C T N",700,150);
+fill("orange");
+text("Hint: Always perform a particular task!!",700,180);
+
+
+
+
+}
